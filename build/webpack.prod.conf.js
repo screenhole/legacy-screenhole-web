e377624ca6c1bc0058a96e443476a8998f9ba6d1
Jacob Bijani <jacob.bijani@gmail.com>
2017-10-15 16:33:15 -0400

    write the index.html file to 200.html, so surge will serve it

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -60,8 +60,8 @@ const webpackConfig = merge(baseWebpackConfig, {
       template: 'index.html',
       inject: true,
       minify: {
-        removeComments: true,
-        collapseWhitespace: true,
+        removeComments: false,
+        collapseWhitespace: false,
         removeAttributeQuotes: true
         // more options:
         // https://github.com/kangax/html-minifier#options-quick-reference
