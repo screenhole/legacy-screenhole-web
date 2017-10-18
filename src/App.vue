c3be24db88cbb9b8dbfe9c979172b4f7cb0fb556
Jacob Bijani <jacob.bijani@gmail.com>
2017-10-18 18:17:20 -0400

    move header to PageHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
 <template>
     <section id="app">
-        <header class="main">
-            <router-link class="logo" to="/" exact><img src="./assets/img/logo.svg" alt="SCREENHOLE!"></router-link>
-
-            <nav class="pages">
-                <router-link to="/login">log in</router-link>
-                <a href="https://www.facebook.com/groups/screenhole" target="_blank">talk</a>
-                <a href="https://screenhole.net/pasquale/~9B5spw">wtf?</a>
-                <a href="https://twitter.com/screenhole">twitter</a>
-            </nav>
-        </header>
+        <page-header></page-header>
 
         <router-view/>
 
@@ -18,8 +9,13 @@
 </template>
 
 <script>
+import PageHeader from '@/components/PageHeader'
+
 export default {
-    name: 'app'
+    name: 'app',
+    components: {
+        PageHeader
+    }
 }
 </script>
 
@@ -42,41 +38,4 @@ export default {
         width: 150px;
         display: block;
     }
-</style>
-
-<style lang="scss">
-header.main {
-    display: flex;
-    justify-content: space-between;
-    z-index: 10;
-    background-color: #000;
-    position: fixed;
-    height: 60px;
-    width: 100%;
-    padding: 20px;
-    padding-top: 18px;
-
-    .logo {
-        display: flex;
-    }
-
-    nav.pages {
-        display: flex;
-
-        a {
-            color: #6A40EE; // todo: $purple variable
-            // font-weight: bold;
-            margin-left: 50px;
-            text-decoration: none;
-            display: flex;
-
-            &:hover {
-              color: #3ae06e; // todo: $bright-green
-            }
-            &.router-link-active {
-                color: white;
-            }
-        }
-    }
-}
 </style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,51 @@
+<template>
+    <header class="main">
+        <router-link class="logo" to="/" exact>
+            <img src="../assets/img/logo.svg" alt="SCREENHOLE!">
+        </router-link>
+
+        <nav class="pages">
+            <router-link to="/login">log in</router-link>
+            <a href="https://www.facebook.com/groups/screenhole" target="_blank">talk</a>
+            <a href="https://screenhole.net/pasquale/~9B5spw">wtf?</a>
+            <a href="https://twitter.com/screenhole">twitter</a>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+.main {
+    display: flex;
+    justify-content: space-between;
+    z-index: 10;
+    background-color: #000;
+    position: fixed;
+    height: 60px;
+    width: 100%;
+    padding: 20px;
+    padding-top: 18px;
+
+    .logo {
+        display: flex;
+    }
+
+    .pages {
+        display: flex;
+
+        a {
+            color: #6A40EE; // todo: $purple variable
+            // font-weight: bold;
+            margin-left: 50px;
+            text-decoration: none;
+            display: flex;
+
+            &:hover {
+              color: #3ae06e; // todo: $bright-green
+            }
+            &.router-link-active {
+                color: white;
+            }
+        }
+    }
+}
+</style>
