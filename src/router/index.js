32bd43cd6730105268b047be136370e7700cb778
Jacob Bijani <jacob.bijani@gmail.com>
2017-10-17 23:34:52 -0400

    proper beanman. one up, tiled

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Stream from '@/components/Stream'
 import Login from '@/components/Login'
 import UserStream from '@/components/UserStream'
 import GrabPermalink from '@/components/GrabPermalink'
+import Beanman from '@/components/Beanman'
 
 Vue.use(Router)
 
@@ -13,6 +14,7 @@ export default new Router({
         // higher on list = higher priority route (aka first match wins)
         { path: '/', component: Stream },
         { path: '/login', component: Login },
+        { path: '/~beanman', component: Beanman},
         { path: '/:username', name: 'user-stream', component: UserStream },
         { path: '/:username/~:grab_id', name: 'grab-permalink', component: GrabPermalink },
     ]
