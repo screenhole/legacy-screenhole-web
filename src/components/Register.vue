4ea5af0e19f49599da96c17df788c63af0d995bc
Jacob Bijani <jacob.bijani@gmail.com>
2017-10-19 19:16:40 -0400

    add Register vue

---
+++ src/components/Register.vue
@@ -0,0 +1,155 @@
+<template>
+    <div class="centeredForm">
+        <form id="login" v-on:submit.prevent="submitLogin">
+            <h1>Register</h1>
+
+            <input type="email" placeholder="email" v-model="email">
+            <input type="text" placeholder="username" v-model="username">
+            <input type="password" placeholder="password" v-model="password">
+
+            <button type="submit">GO!</button>
+        </form>
+
+        <div v-if="terminal" v-bind:class="{ flash: terminal }" v-clipboard:copy="terminal">
+            <pre>{{terminal}}</pre>
+        </div>
+    </div>
+</template>
+
+<script>
+// TODO: hide mr hole
+export default {
+    data () {
+        return {
+            email: '',
+            username: '',
+            password: '',
+            jwt: '',
+            terminal: '',
+        };
+    },
+    methods: {
+        submitLogin: function() {
+            if (! this.email || ! this.username || ! this.password) {
+                return;
+            }
+
+            this.$http.post("/users", {
+                auth: {
+                    email: this.email,
+                    username: this.username,
+                    password: this.password,
+                }
+            }).then((response) => {
+                // success, show JWT
+                this.jwt = response.data.jwt;
+                this.showTerminalJWT();
+            }, response => {
+                // register failure. show the error.
+                this.terminal = 'oops, ' + this.username + '. couldn\'t register.';
+            });
+        },
+        showTerminalJWT: function () {
+            window.location = 'screenhole:///jwt/' + this.jwt;
+            this.terminal = 'defaults write com.thinko.screenhole.macos "jwt" "' + this.jwt + '"';
+        },
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~resources";
+
+.centeredForm {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    h1 {
+        color: white;
+        margin-bottom: 25px;
+        font-size: 50px;
+        font-weight: bold;
+    }
+
+    input {
+        width: 300px;
+        display: block;
+        height: 50px;
+        padding: 0;
+        font-size: 25px;
+        border: 0;
+        border-bottom: 2px solid $grey-cool;
+        background-color: transparent;
+        transition: all 0.2s ease;
+
+        &, &::placeholder {
+            color: $grey-cool;
+        }
+
+        &:focus {
+            color: #fff;
+            border-color: #fff;
+            outline: none;
+        }
+    }
+
+    button {
+        width: 300px;
+        height: 50px;
+        margin-top: 25px;
+        padding: 0;
+        border: 0;
+        background: center no-repeat transparent url("../assets/img/form-button.svg");
+        background-size: 300px 50px;
+        color: $purple;
+        font-size: 16px;
+        font-weight: bold;
+        transition: all 0.2s ease;
+
+        &:active {
+            transform: scale(0.95) translate(0, 2px);
+            outline: none;
+        }
+
+        &:focus {
+            outline: none;
+        }
+    }
+
+    @keyframes slideup {
+        from {bottom: -200px;}
+        to {bottom: 0;}
+    }
+
+    @keyframes slidedown {
+        from {bottom: 0;}
+        to {bottom: -200px;}
+    }
+
+    .flash, .flash pre {
+        color: #fff;
+        position: fixed;
+        width: 100%;
+        margin: 0;
+        padding: 25px;
+        bottom: 0;
+        left: 0;
+        background-color: $purple;
+        animation-name: slideup;
+        animation-duration: 1s;
+        animation-timing-function: ease-in-out;
+    }
+
+
+    #login {
+        width: 300px;
+        padding-bottom: 20vh; // offset height for optics
+    }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Stream from '@/components/Stream'
 import Login from '@/components/Login'
+import Register from '@/components/Register'
 import UserStream from '@/components/UserStream'
 import GrabPermalink from '@/components/GrabPermalink'
 import Beanman from '@/components/Beanman'
@@ -15,6 +16,7 @@ export default new Router({
         // higher on list = higher priority route (aka first match wins)
         { path: '/', component: Stream },
         { path: '/login', component: Login },
+        { path: '/register', component: Register },
         { path: '/~beanman', component: Beanman},
         { path: '/:username', name: 'user-stream', component: UserStream },
         { path: '/:username/~:grab_id', name: 'grab-permalink', component: GrabPermalink },
