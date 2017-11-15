ec777d416dd64586f6a0fb704195641183f217ff
Jacob Bijani <jacob.bijani@gmail.com>
2017-11-15 11:27:39 -0500

    set port, sendFile

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 8080))
 
 app.use(express.static(__dirname + '/dist'))
 
 app.get('*', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile('200.html', { root: 'dist' });
 })
 
 app.listen(app.get('port'), () => {
