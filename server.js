774c11d7d16eb5614fcb07a80f9cdc279965cc8d
Jacob Bijani <jacob.bijani@gmail.com>
2017-11-15 11:55:58 -0500

    set up router middleware

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require('express')
 const app = express()
-
-app.set('port', (process.env.PORT || 8080))
+const router = express.Router()
 
 app.use(express.static(__dirname + '/dist'))
 
-app.get('*', (req, res) => {
+// router.use(function (req, res, next) {
+//     console.log('Time:', Date.now())
+//     next()
+// })
+
+router.get('/:username/~:shot_id', function (req, res, next) {
+    console.log(req.params['username'])
+    // pass through
+    next()
+})
+
+router.get('*', (req, res) => {
     res.sendFile('200.html', { root: 'dist' });
 })
 
-app.listen(app.get('port'), () => {
-    console.log('Node app is running on port', app.get('port'))
+app.use(router)
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+    console.log('Node app is running on port', PORT)
 })
