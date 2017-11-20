a330c909ed1dcf05e09566a340c86f04098f4aa9
Jacob Bijani <jacob.bijani@gmail.com>
2017-11-20 14:57:22 -0500

    move grab to a component

---
+++ src/components/Grab.vue
@@ -0,0 +1,125 @@
+<template>
+    <div class="grab">
+        <div class="content">
+            <div class="meta">
+                <router-link  class="permalink" v-if="grab.user" :to="{ name: 'user-stream', params: {
+                    username: grab.user.username
+                    }}">
+                    <avatar :user="grab.user"></avatar>
+                    <span class="user">{{grab.user.username}}</span>
+                </router-link>
+            </div>
+            <router-link v-if="grab.user" :to="{ name: 'grab-permalink', params: {
+                username: grab.user.username,
+                grab_id: grab.id
+            }}">
+              <img :src="grab.image_public_url" />
+              <div class="shadow"></div>
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+import Avatar from '@/components/Avatar';
+
+export default {
+    props: [ 'grab' ],
+
+    components: {
+        Avatar,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~resources";
+.grab {
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+    padding-bottom: 100px;
+
+
+    .content {
+        max-width: 1200px;
+        flex-grow: 1;
+        position: relative;
+
+        .permalink {
+            &:hover {
+                .user {
+                    color: $purple;
+                    background-color: white;
+
+                    &:after {
+                        border-color: transparent white transparent transparent;
+                    }
+                }
+            }
+        }
+        .meta {
+            margin-bottom: 10px;
+            border-radius: 1000px;
+            display: inline-block;
+
+            .avatar {
+                display: inline-block;
+                margin: 0;
+                float: left;
+            }
+
+            .user {
+                background-color: $purple;
+                padding: 5px 10px;
+                border-radius: 100px;
+                display: inline-block;
+                margin-top: 5px;
+                margin-left: 10px;
+                color: white;
+                transition: all 0.2s ease;
+                float: left;
+
+                &:after {
+                    content: '';
+                    position: absolute;
+                    top: 14px;
+                    left: 38px;
+                    width: 0;
+                    height: 0;
+                    border-style: solid;
+                    border-width: 5px 10px 5px 0;
+                    border-color: transparent $purple transparent transparent;
+                    transition: all 0.2s ease;
+                }
+                &:hover {
+                    color: $purple;
+                    background-color: white;
+
+                    &:after {
+                        border-color: transparent white transparent transparent;
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    img {
+        clear: both;
+        display: block;
+        max-width: 100%;
+        border-radius: 5px;
+        transition: all 0.1s ease;
+        max-height: 80vh;
+        border: 1px solid rgba(255,255,255,0.1);
+
+        &:hover {
+            // border: 1px solid $purple;
+            box-shadow: 0px 0px 0px 5px $purple;
+        }
+
+    }
+}
+</style>
--- src/components/Stream.vue
+++ src/components/Stream.vue
@@ -1,24 +1,11 @@
 <template>
     <div class="stream">
-        <div class="grab" v-for="grab in grabs" v-if="grabs && grabs.length">
-            <div class="content">
-                <div class="meta">
-                    <router-link  class="permalink" v-if="grab.user" :to="{ name: 'user-stream', params: {
-                        username: grab.user.username
-                        }}">
-                        <avatar :user="grab.user"></avatar>
-                        <span class="user">{{grab.user.username}}</span>
-                    </router-link>
-                </div>
-                <router-link v-if="grab.user" :to="{ name: 'grab-permalink', params: {
-                    username: grab.user.username,
-                    grab_id: grab.id
-                }}">
-                  <img :src="grab.image_public_url" />
-                  <div class="shadow"></div>
-                </router-link>
-            </div>
-        </div>
+        <grab
+            v-for="(grab, index) in grabs"
+            v-bind:key="grab.id"
+            v-bind:grab="grab"
+            v-on:remove="grabs.splice(index, 1)"
+        />
         <infinite-loading @infinite="infiniteHandler">
             <div slot="spinner">
                 <div id="loader"></div>
@@ -33,7 +20,7 @@
 import InfiniteLoading from 'vue-infinite-loading';
 import ActionCable from 'actioncable';
 
-import Avatar from '@/components/Avatar';
+import Grab from '@/components/Grab';
 
 export default {
     data () {
@@ -77,14 +64,16 @@ export default {
         );
     },
     components: {
-        Avatar,
         InfiniteLoading,
+
+        Grab,
     },
 }
 </script>
 
 <style lang="scss" scoped>
 @import "~resources";
+
 .stream {
     display: flex;
     padding: 50px 50px 100px 50px;
@@ -92,94 +81,5 @@ export default {
     flex-direction: column;
     background-color: black;
     margin: 0 auto;
-
-    .grab {
-        display: flex;
-        justify-content: center;
-        flex-direction: row;
-        padding-bottom: 100px;
-
-
-        .content {
-            max-width: 1200px;
-            flex-grow: 1;
-            position: relative;
-
-            .permalink {
-                &:hover {
-                    .user {
-                        color: $purple;
-                        background-color: white;
-
-                        &:after {
-                            border-color: transparent white transparent transparent;
-                        }
-                    }
-                }
-            }
-            .meta {
-                margin-bottom: 10px;
-                border-radius: 1000px;
-                display: inline-block;
-
-                .avatar {
-                    display: inline-block;
-                    margin: 0;
-                    float: left;
-                }
-
-                .user {
-                    background-color: $purple;
-                    padding: 5px 10px;
-                    border-radius: 100px;
-                    display: inline-block;
-                    margin-top: 5px;
-                    margin-left: 10px;
-                    color: white;
-                    transition: all 0.2s ease;
-                    float: left;
-
-                    &:after {
-                        content: '';
-                        position: absolute;
-                        top: 14px;
-                        left: 38px;
-                        width: 0;
-                        height: 0;
-                        border-style: solid;
-                        border-width: 5px 10px 5px 0;
-                        border-color: transparent $purple transparent transparent;
-                        transition: all 0.2s ease;
-                    }
-                    &:hover {
-                        color: $purple;
-                        background-color: white;
-
-                        &:after {
-                            border-color: transparent white transparent transparent;
-                        }
-                    }
-                }
-
-            }
-
-        }
-
-        img {
-            clear: both;
-            display: block;
-            max-width: 100%;
-            border-radius: 5px;
-            transition: all 0.1s ease;
-            max-height: 80vh;
-            border: 1px solid rgba(255,255,255,0.1);
-
-            &:hover {
-                // border: 1px solid $purple;
-                box-shadow: 0px 0px 0px 5px $purple;
-            }
-
-        }
-    }
 }
 </style>
