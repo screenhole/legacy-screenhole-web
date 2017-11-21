b6607e2de63c7410261689d913ff4aad792fe4ba
Jacob Bijani <jacob.bijani@gmail.com>
2017-11-21 14:52:34 -0500

    move wtf assets to s3 bucket

--- src/components/Wtf.vue
+++ src/components/Wtf.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="content">
-        <img src="/static/img/wtf.png" alt="MR. HOLE">
-        <img src="../assets/img/how-to.png" alt="how to">
-        <img src="../assets/img/invite.png" alt="invite?">
+        <img src="//screenhole.s3.amazonaws.com/wtf/wtf.png" alt="wtf">
+        <img src="//screenhole.s3.amazonaws.com/wtf/how-to.png" alt="how to">
+        <img src="//screenhole.s3.amazonaws.com/wtf/invite.png" alt="invite?">
     </div>
 
 </template>
@@ -13,19 +13,16 @@
 <style lang="scss" scoped>
 @import "~resources";
 
-.content{
-    // background-color: red;
-        margin: 50px auto;
-        padding: 50px 10px;
-        max-width: 1000px;
+.content {
+    margin: 50px auto;
+    padding: 50px 10px;
+    max-width: 1000px;
 
     img {
         display: block;
         max-width: 100%;
         border-radius: 5px;
-
     }
-
 }
 
 </style>
