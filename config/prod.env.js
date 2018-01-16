553a51c56abb059b3e8c9ba1d1c1c52eaf9a66bd
Jacob Bijani <jacob.bijani@gmail.com>
2018-01-16 12:15:00 -0500

    put api back

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
-  API_BASE: process.env.API_BASE ? '"' + process.env.API_BASE + '"' : '"https://screenhole-api.herokuapp.com"',
+  API_BASE: process.env.API_BASE ? '"' + process.env.API_BASE + '"' : '"https://api.screenhole.net"',
   NODE_ENV: '"production"'
 }
