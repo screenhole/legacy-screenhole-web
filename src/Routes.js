74b418045d79c851b27f1da1e0b202e0bed47571
Wojtek Witkowski <hi@wojtek.im>
2018-01-19 21:27:26 +0100

    settings view layout

--- src/Routes.js
+++ src/Routes.js
@@ -6,6 +6,7 @@ import GrabSingle from './views/GrabSingle/GrabSingle';
 import UserStream from './views/UserStream/UserStream';
 import Manual from './views/Manual/Manual';
 import NotFound from './views/NotFound/NotFound';
+import Settings from './views/Settings/Settings';
 
 class Routes extends Component {
   render() {
@@ -14,6 +15,7 @@ class Routes extends Component {
         <Route exact path="/" component={GrabStream} />
         <Route exact path="/manual" component={Manual} />
         <Route exact path="/wtf" component={Manual} />
+        <Route exact path="/settings" component={Settings} />
         <Route exact path="/:username" component={UserStream} />
         <Route exact path="/:username/:id" component={GrabSingle} />
         <Route path="*" component={NotFound} />
