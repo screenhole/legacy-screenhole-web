7797c585d0d418eda529943cc27eea8d07820667
Jacob Bijani <jacob.bijani@gmail.com>
2018-02-15 14:35:31 -0500

    lint api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,41 +1,41 @@
-import { create } from 'apisauce'
+import { create } from 'apisauce';
 
 const api = create({
   baseURL: 'https://api.screenhole.net',
   // baseURL: 'https://screenhole-api.ngrok.io',
   // baseURL: 'https://staging-api.screenhole.net',
-})
+});
 
 // reset on 401 API responses
 api.addResponseTransform(response => {
-  if (! response.ok) {
+  if (!response.ok) {
     if (response.status === 401) {
       api.resetLocalStorage();
       window.location = window.location;
     }
   }
-})
+});
 
 api.currentUser = null;
 api.authenticated = false;
 
-api.setCurrentUser = (user) => {
+api.setCurrentUser = user => {
   localStorage.setItem('user_current', JSON.stringify(user));
 
   api.currentUser = user;
-}
+};
 
-api.setAuthHeader = (token) => {
+api.setAuthHeader = token => {
   localStorage.setItem('user_token', token);
   api.setHeader('Authorization', `Bearer ${token}`);
 
-  api.authenticated = !! token;
-}
+  api.authenticated = !!token;
+};
 
 api.resetLocalStorage = () => {
   localStorage.removeItem('user_token');
   localStorage.removeItem('user_current');
-}
+};
 
 if (localStorage.getItem('user_token')) {
   api.setAuthHeader(localStorage.getItem('user_token'));
