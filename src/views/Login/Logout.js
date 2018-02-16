67755552b2ac0aec9f9a72b05c35d66cd3f3fdd6
Jacob Bijani <jacob.bijani@gmail.com>
2018-02-16 12:40:56 -0500

    lint this file

--- src/views/Login/Logout.js
+++ src/views/Login/Logout.js
@@ -6,12 +6,10 @@ class Logout extends Component {
   componentWillMount = () => {
     api.resetLocalStorage();
     window.location = '/';
-  }
+  };
 
   render() {
-    return (
-      <div />
-    );
+    return <div />;
   }
 }
 
