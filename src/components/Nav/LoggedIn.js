3d690a1654f1ae3e4ce917e6cbec7464cc7cef08
psql <accounts@psql.me>
2018-02-21 17:38:24 -0500

    Download links
    Named it /apps

--- src/components/Nav/LoggedIn.js
+++ src/components/Nav/LoggedIn.js
@@ -1,93 +1,91 @@
-import React, { Component, Fragment } from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import styled from 'styled-components';
+import React, { Component, Fragment } from "react";
+import { Link, NavLink } from "react-router-dom";
+import styled from "styled-components";
 
-import Avatar from '../User/Avatar';
-import Buttcoin from '../Buttcoin/Buttcoin';
+import Avatar from "../User/Avatar";
+import Buttcoin from "../Buttcoin/Buttcoin";
 
 class LoggedIn extends Component {
-  render() {
-    const { buttcoins, username, gravatar_hash } = this.props;
+    render() {
+        const { buttcoins, username, gravatar_hash } = this.props;
 
-    return (
-      <Fragment>
-        <Buttcoin amount={buttcoins} />
-        <a
-          href="https://news.screenhole.net"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          news
-        </a>
-        <a
-          href="https://twitter.com/screenhole"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          twitter
-        </a>
-        <NavLink to="/wtf">wtf</NavLink>
-        <Menu className="nav-dropdown">
-          <Avatar
-            gravatar={gravatar_hash}
-            username={username}
-          />
-          <Dropdown className="user-nav-dropdown">
-            <Link to={`/${username}`}>@{username}</Link>
-            <NavLink to="/settings">settings</NavLink>
-            <Link to="/logout">log out</Link>
-          </Dropdown>
-        </Menu>
-      </Fragment>
-    );
-  }
+        return (
+            <Fragment>
+                <Buttcoin amount={buttcoins} />
+                <a
+                    href="https://news.screenhole.net"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    news
+                </a>
+                <a
+                    href="https://twitter.com/screenhole"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    twitter
+                </a>
+                <NavLink to="/wtf">wtf</NavLink>
+                <NavLink to="/apps">apps</NavLink>
+                <Menu className="nav-dropdown">
+                    <Avatar gravatar={gravatar_hash} username={username} />
+                    <Dropdown className="user-nav-dropdown">
+                        <Link to={`/${username}`}>@{username}</Link>
+                        <NavLink to="/settings">settings</NavLink>
+                        <Link to="/logout">log out</Link>
+                    </Dropdown>
+                </Menu>
+            </Fragment>
+        );
+    }
 }
 
 export default LoggedIn;
 
 const Menu = styled.div`
-  position: relative;
-  padding-left: 3rem;
-  padding-bottom: 1rem;
-  top: 0.5rem;
+    position: relative;
+    padding-left: 3rem;
+    padding-bottom: 1rem;
+    top: 0.5rem;
 
-  &:hover {
-    .user-nav-dropdown {
-      animation: jellyReveal 0.65s linear both;
+    &:hover {
+        .user-nav-dropdown {
+            animation: jellyReveal 0.65s linear both;
+        }
     }
-  }
 `;
 
 const Dropdown = styled.div`
-  z-index: 3000;
-  position: absolute;
-  top: 0;
-  right: 0;
-  background-color: #6a40ee;
-  border-radius: 5px;
-  margin-top: 2.75rem;
-  padding: 0.25rem 0;
-  box-shadow: 0 10px 35px 0 rgba(0, 0, 0, 0.35);
-  opacity: 0;
-  visibility: hidden;
-  transition: all 0.4s ease;
-  transform-origin: 80% -20%;
-
-  &::after {
-    content: '';
+    z-index: 3000;
     position: absolute;
-    top: -8px;
-    right: 10px;
-    width: 0;
-    height: 0;
-    border-style: solid;
-    border-width: 0 7.5px 10px;
-    border-color: transparent transparent #6a40ee;
-  }
+    top: 0;
+    right: 0;
+    background-color: #6a40ee;
+    border-radius: 5px;
+    margin-top: 2.75rem;
+    padding: 0.25rem 0;
+    box-shadow: 0 10px 35px 0 rgba(0, 0, 0, 0.35);
+    opacity: 0;
+    visibility: hidden;
+    transition: all 0.4s ease;
+    transform-origin: 80% -20%;
 
-  a {
-    color: #fff;
-    display: block;
-    padding: 0.5rem 1rem;
-  }
+    &::after {
+        content: "";
+        position: absolute;
+        top: -8px;
+        right: 10px;
+        width: 0;
+        height: 0;
+        border-style: solid;
+        border-width: 0 7.5px 10px;
+        border-color: transparent transparent #6a40ee;
+    }
+
+    a {
+        color: #fff;
+        display: block;
+        padding: 0.5rem 1rem;
+    }
 `;
---
+++ src/views/Static/Apps.js
@@ -0,0 +1,117 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import Helmet from "react-helmet";
+
+export default class apps extends Component {
+    render() {
+        return (
+            <Page>
+                <section>
+                    <h1>Download the apps</h1>
+                    <p>Use our apps to post to post grabs to the 'hole.</p>
+                    <img
+                        class="download"
+                        src="/img/apps.png"
+                        alt="iPhone App"
+                    />
+                    <p class="links">
+                        <a
+                            href="http://appstore.com/screenhole"
+                            target="_blank"
+                        >
+                            iPhone
+                        </a>{" "}
+                        /{" "}
+                        <a
+                            href="https://rink.hockeyapp.net/apps/df7fde32da044e62980cfb683cb7d0b9"
+                            target="_blank"
+                        >
+                            Macintosh
+                        </a>
+                    </p>
+                </section>
+                <Helmet>
+                    {/* Drop meta tags in here */}
+                    <title>Download the apps!</title>
+                    <meta
+                        name="description"
+                        content="<description of this page>"
+                    />
+                    <meta property="og:title" content="Download the apps!" />
+                    <meta
+                        property="og:url"
+                        content="https://screenhole.net/download"
+                    />
+                    <meta
+                        property="og:description"
+                        content="<description of this page>"
+                    />
+                    {/* Make the url for this image absolute */}
+                    <meta property="og:image" content="<url for the image>" />
+                    <meta name="twitter:card" content="summary_large_image" />
+                    <meta name="twitter:site" content="@screenhole" />
+                    <meta name="twitter:creator" content="@screenhole" />
+                </Helmet>
+            </Page>
+        );
+    }
+}
+
+const Page = styled.div`
+    margin: 0 auto 50px auto;
+    padding: 50px;
+    @media (max-width: 790px) {
+        padding: 0;
+    }
+    max-width: 1000px;
+    section {
+        color: #858090;
+        line-height: 150%;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        & + section {
+            margin-top: 100px;
+        }
+        h1 {
+            margin-top: 5vh;
+        }
+        h1 + h2 {
+            margin-top: 3rem;
+        }
+        p + h2,
+        p + h1 {
+            margin-top: 5rem;
+        }
+        p {
+            max-width: 640px;
+            margin-top: 1.5rem;
+            text-align: center;
+        }
+        code {
+            display: inline-block;
+            clear: both;
+            background-color: rgba(255, 255, 255, 0.1);
+            padding: 0.15em 0.25em;
+        }
+        a {
+            color: $purple;
+        }
+    }
+    img {
+        display: block;
+        max-width: 100%;
+        border-radius: 5px;
+    }
+
+    .download {
+        width: 50%;
+        margin-top: 100px;
+    }
+    .links {
+        font-size: 200%;
+    }
+`;
