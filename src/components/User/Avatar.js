554f380d110312857bd166047b797791788ec8c0
Wojtek Witkowski <hi@wojtek.im>
2019-05-01 02:30:26 +0200

    we don't need this anymore

--- src/components/User/Avatar.js
+++ src/components/User/Avatar.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-import phone from "./icon-phone-chomment.svg";
-
 import defaultAvatar from "./default-avatar.png";
 
 class Avatar extends Component {
@@ -31,11 +29,6 @@ class Avatar extends Component {
           alt={this.props.username}
           className="user-avatar"
         />
-        {this.props.variant === "voice_memo" && (
-          <PhoneIcon>
-            <Icon src={phone} />
-          </PhoneIcon>
-        )}
       </Link>
     );
   }
@@ -49,27 +42,4 @@ const Image = styled.img`
   display: block;
   object-fit: cover;
   background-color: #111;
-`;
-
-const PhoneIcon = styled.span`
-  position: absolute;
-  background-color: var(--primary-color);
-  border-radius: 100%;
-  width: 1.25rem;
-  height: 1.25rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 0.25rem;
-  transform: translateY(-65%) translateX(-25%);
-  box-shadow: 0 0 0 3px var(--body-bg-color);
-  pointer-events: none;
-  outline: none;
-`;
-
-const Icon = styled.img`
-  background-color: transparent !important;
-  width: 100% !important;
-  height: 100% !important;
-  margin: 0 !important;
 `;
